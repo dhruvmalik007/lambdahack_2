524a0aa4040a058ad1900a64ec4c6008224ae4cb
Gregory Edison <gregory.edison1993@gmail.com>
2024-07-07 18:29:41 +0200

    commit to guesses and result

--- mine/src/main.rs
+++ mine/src/main.rs
@@ -8,27 +8,34 @@
 
 sp1_zkvm::entrypoint!(main);
 
-pub fn main() {
-    // Read an input to the program.
-    // Behind the scenes, this compiles down to a custom system call which handles reading inputs
-    // from the prover.
-    // set initial program state
-    let max_tries = 10;
+use alloy_sol_types::{sol, SolValue};
 
+sol! {
+    /// Commitment to the game state
+    struct GameCommitment {
+        tuple(uint8, uint8)[] user_guesses;
+        /// Whether the game is won or not
+        bool result;
+    }
+}
+
+pub fn main() {
+    // Read inputs to the program.
     let size = sp1_zkvm::io::read::<u8>();
     let stored_bombs = sp1_zkvm::io::read::<Vec<(u8, u8)>>();
     let user_guesses = sp1_zkvm::io::read::<Vec<(u8, u8)>>();
+
+    // Check if the number of bombs is within the bounds
+    let max_bombs = (size as f32).sqrt() as u8;
+    if stored_bombs.len() > max_bombs as usize {
+        panic!("Too many bombs");
+    }
 
     // Verify bounds
     for ((b_x, b_y), (u_x, u_y)) in stored_bombs.iter().zip(user_guesses.iter()) {
         if *b_x > size - 1 || *b_y > size - 1 || *u_x > size - 1 || *u_y > size - 1 {
             panic!("Out of bounds");
         }
-    }
-
-    // Check if the user has tried too many times
-    if user_guesses.len() > max_tries {
-        panic!("Too many tries");
     }
 
     // Iterate over the tries and check if the user has hit a bomb
@@ -40,5 +47,11 @@ pub fn main() {
         }
     }
 
-    sp1_zkvm::io::commit(&hit);
+    let bytes = GameCommitment {
+        result: !hit,
+        user_guesses,
+    }
+    .abi_encode();
+
+    sp1_zkvm::io::commit(&bytes);
 }
