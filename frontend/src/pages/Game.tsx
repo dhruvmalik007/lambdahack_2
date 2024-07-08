1e31f5b379472bb84a174f194f41dedc9e269a81
Pol <polmonteroalegre@gmail.com>
2024-07-08 19:15:38 +0200

    FEAT: win, lose states

--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -93,13 +93,35 @@ const Game = () => {
         }
     }, [state]);
 
-    const betFunction = async () => {
-        const response = await getTest();
-        console.log(response.body);
+    const delay = (ms: number) =>
+        new Promise((resolve) => setTimeout(resolve, ms));
+
+    const sendBet = async () => {
+        const positivefeedback = document.getElementById('positivefeedback');
+        const negativefeedback = document.getElementById('negativefeedback');
+        if (Math.random() < 0.3) {
+            const jsConfetti = new JSConfetti();
+            for (let i = 0; i < 6; i++) {
+                positivefeedback.innerHTML == 'WIN'
+                    ? (positivefeedback.innerHTML = '')
+                    : (positivefeedback.innerHTML = 'WIN');
+                jsConfetti.addConfetti();
+                await delay(200);
+            }
+        } else {
+            for (let i = 0; i < 6; i++) {
+                negativefeedback.innerHTML == 'LOSE'
+                    ? (negativefeedback.innerHTML = '')
+                    : (negativefeedback.innerHTML = 'LOSE');
+                await delay(200);
+            }
+        }
     };
 
     return (
         <>
+            <div id="positivefeedback"></div>
+            <div id="negativefeedback"></div>
             <div className="App">
                 <div className="infoBar">
                     <button
@@ -121,7 +143,17 @@ const Game = () => {
                     <br />
                     <br />
                     <br />
-                    <button>BET</button>
+                    <button
+                        ref={restartBtn}
+                        onClick={() => {
+                            sendBet();
+                            setState('waiting');
+                            setUI({ time: 0, flags: difficulty.mines });
+                            localStorage.setItem('isRestart', true.toString());
+                        }}
+                    >
+                        BET
+                    </button>
                     <br />
                     <div className="">
                         <input
