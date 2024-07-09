30c3861b0f6bcf07478623ef3ae71b24bf0e530e
Pol <polmonteroalegre@gmail.com>
2024-07-09 10:10:50 +0200

    basic abi calls

--- frontend/src/services/abiCalls.ts
+++ frontend/src/services/abiCalls.ts
@@ -1,6 +1,8 @@
 const PORT = process.env.PORT || 5000;
 const URL_HOST = process.env.URL_HOST || "localhost";
 
+// get game id from localstorage, get that id when initziate game
+
 const callGuessMethod = async () => {
     const guess = await fetch(
         `https://${URL_HOST}:${PORT}`,
@@ -17,4 +19,24 @@ const callGuessMethod = async () => {
             }),
         }
     );
+}
+
+
+const submitGuessMethod = async () => {
+    const bet = localStorage.getItem("bet")
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ bet }),
+    }
+
+    try {
+        const response = await fetch(`https://${URL_HOST}:${PORT}`, options);
+        return response
+    } catch (error) {
+        return error
+    }
+
 }
