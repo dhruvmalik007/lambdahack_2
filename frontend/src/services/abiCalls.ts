5bc598e786ddf0216f1e020881cac2f54162738a
Pol <polmonteroalegre@gmail.com>
2024-07-09 10:28:11 +0200

    wallet fix for connecting to the contract

--- frontend/src/services/abiCalls.ts
+++ frontend/src/services/abiCalls.ts
@@ -3,24 +3,23 @@ const URL_HOST = process.env.URL_HOST || "localhost";
 
 // get game id from localstorage, get that id when initziate game
 
-const callGuessMethod = async () => {
-    const guess = await fetch(
-        `https://${URL_HOST}:${PORT}`,
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                //difficulty: difficulty.name,
-                //time: ui.time,
-                //flags: ui.flags,
-                //mines: difficulty.mines,
-            }),
-        }
-    );
-}
+const startGameMethod = async () => {
+    const bet = localStorage.getItem("bet")
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ bet }),
+    }
 
+    try {
+        const response = await fetch(`https://${URL_HOST}:${PORT}`, options);
+        return response
+    } catch (error) {
+        return error
+    }
+}
 
 const submitGuessMethod = async () => {
     const bet = localStorage.getItem("bet")
@@ -38,5 +37,4 @@ const submitGuessMethod = async () => {
     } catch (error) {
         return error
     }
-
 }
